016b9ac307b80304a3bbd20f36870f98a3958c70
lazybensch <lazybensch@gmail.com>
2015-11-17 12:57:47 +0100

    scaffold component

---
+++ addon/components/deep-select.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import layout from '../templates/components/deep-select';
+const { Component } = Ember;
+
+export default Component.extend({
+  classNames: ['deep-select'],
+  layout: layout,
+});
