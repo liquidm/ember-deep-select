1017473d9336d649398ffb7a352503fa26aa36fe
lazybensch <lazybensch@gmail.com>
2015-11-17 13:04:22 +0100

    add interactions

--- addon/components/deep-select.js
+++ addon/components/deep-select.js
@@ -1,8 +1,38 @@
 import Ember from 'ember';
 import layout from '../templates/components/deep-select';
-const { Component } = Ember;
+import filterByQuery from 'ember-cli-filter-by-query';
+import { keyDown } from 'ember-keyboard';
+import without from 'lodash/array/without';
+const { Component, computed, on } = Ember;
 
 export default Component.extend({
   classNames: ['deep-select'],
   layout: layout,
+  keyboard: Ember.inject.service(),
+
+  options: computed('content.options.[]', 'content.selections.[]', function() {
+    const selections = this.get('content.selections');
+    const options = this.get('content.options');
+    return without(options, ...selections);
+  }),
+
+  focus: on('didInsertElement', function() {
+    this.$('input').focus();
+  }),
+
+  select(option) {
+    const selections = this.get('content.selections');
+    selections.pushObject(option);
+    this.set('query', '');
+  },
+
+  deselect(option) {
+    const selections = this.get('content.selections');
+    selections.removeObject(option);
+  },
+
+  actions: {
+    select(option)   { this.select(option);   },
+    deselect(option) { this.deselect(option); }
+  }
 });
